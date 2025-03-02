3ec6f581c9140b305020e8e76d2b91ac538a9f62
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-01 21:50:56 -0300

    feat: adding posts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header"
 import Sidebar from "./components/Sidebar"
-import Post from "./Post"
+import Post from "./components/Post"
 
 import "./global.css"
 import styles from "./App.module.css"
@@ -10,20 +10,12 @@ function App() {
   return (
     <div>
       <Header />
-      {/* reutilização de componentes */}
 
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure nihil quasi! Ea eligendi doloremque omnis modi molestias natus cumque, quaerat cupiditate reiciendis eius deleniti nobis nam placeat assumenda illum."
-          />
-
-          <Post
-            author="Lucas Santos"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure nihil quasi! Ea eligendi doloremque omnis modi molestias natus cumque, quaerat cupiditate reiciendis eius deleniti nobis nam placeat assumenda illum."
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import styles from "./Post.module.css"
+
+const Post = () => {
+    return (
+        <article className={styles.post}>
+            <header>
+
+                {/* primeira parte do header */}
+                <div className={styles.author}>
+                    <img
+                        src="https://64.media.tumblr.com/eff450b860c3f2c2fb6006cd754ae49c/c3d375d01875d8c6-8a/s1280x1920/b0ae516254203f1d4015061377b5044d0fd8a9d7.jpg"
+                        className={styles.avatar}
+                    />
+
+                    <div className={styles.authorInfo}>
+                        <strong>Michaelle Oliveira</strong>
+                        <span>Front-end Developer</span>
+                    </div>
+                </div>
+
+                {/* segunda parte do header */}
+                <time title='01 de março, às 21h19' dateTime="2024-01-03 21:19:34">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉{" "} <a href='#'>jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto</a>{" "} 
+                    <a href="">#nlw</a>{" "} 
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
+
+export default Post

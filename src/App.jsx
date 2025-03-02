7a401f0c8bc4b3fce7a46b70ad97cb4534bacf03
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-01 21:07:11 -0300

    feat: creating and finishing sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Header } from "./components/Header"
+import Sidebar from "./components/Sidebar"
 import Post from "./Post"
 
 import "./global.css"
+import styles from "./App.module.css"
 
 function App() {
 
@@ -10,15 +12,20 @@ function App() {
       <Header />
       {/* reutilização de componentes */}
 
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure nihil quasi! Ea eligendi doloremque omnis modi molestias natus cumque, quaerat cupiditate reiciendis eius deleniti nobis nam placeat assumenda illum."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure nihil quasi! Ea eligendi doloremque omnis modi molestias natus cumque, quaerat cupiditate reiciendis eius deleniti nobis nam placeat assumenda illum."
+          />
 
-      <Post 
-        author="Lucas Santos" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure nihil quasi! Ea eligendi doloremque omnis modi molestias natus cumque, quaerat cupiditate reiciendis eius deleniti nobis nam placeat assumenda illum."
-      />
+          <Post
+            author="Lucas Santos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure nihil quasi! Ea eligendi doloremque omnis modi molestias natus cumque, quaerat cupiditate reiciendis eius deleniti nobis nam placeat assumenda illum."
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { PencilLine } from "phosphor-react"
+
+import styles from "./Sidebar.module.css"
+
+const Sidebar = () => {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="" 
+            />
+
+            <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://64.media.tumblr.com/eff450b860c3f2c2fb6006cd754ae49c/c3d375d01875d8c6-8a/s1280x1920/b0ae516254203f1d4015061377b5044d0fd8a9d7.jpg" 
+                />
+                <strong>Michaelle Oliveira</strong>
+                <span>Front-end Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
+
+export default Sidebar
